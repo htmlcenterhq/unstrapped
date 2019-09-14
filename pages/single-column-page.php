bfaf01b365899e17729aa213d1086e23ebf8839b
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2019-09-14 07:37:47 +0100

    adding page templates

--- pages/single-column-page.php
+++ pages/single-column-page.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * Single post template.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Single column page template.
+ * Template Name: Single Column Page
+ * Learn more: https://developer.wordpress.org/themes/template-files-section/page-template-files/
  *
  * @package unstrapped
  */
@@ -51,10 +52,6 @@ get_header();
             
             <div class="col-md-8 blog-main">
           
-                <h3 class="pb-3 mb-4 font-italic border-bottom">
-                    Our Blog Feed
-                </h3>
-
                 <!-- blog posts -->
                 <?php
                 if ( have_posts() ) :
@@ -79,33 +76,6 @@ get_header();
                 ?>
 
             </div><!-- /.blog-main -->
-
-            <aside class="col-md-4 blog-sidebar">
-                <div class="p-3 mb-3 bg-light rounded">
-                    <h4 class="font-italic">About</h4>
-                    <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                </div>
-
-                <div class="p-3">
-                    <h4 class="font-italic">Archives</h4>
-                    <ol class="list-unstyled mb-0">
-                    <li><a href="#">September 2019</a></li>
-                    <li><a href="#">August 2019</a></li>
-                    <li><a href="#">July 2019</a></li>
-                    <li><a href="#">June 2019</a></li>
-                    <li><a href="#">May 2019</a></li>
-                    </ol>
-                </div>
-
-                <div class="p-3">
-                    <h4 class="font-italic">Elsewhere</h4>
-                    <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                    </ol>
-                </div>
-            </aside><!-- /.blog-sidebar -->
 
         </div><!-- /.row -->
 
